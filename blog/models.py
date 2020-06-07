0ef6044018dad796f5c001d0da4971b32af03f62
siddharthkumarmamidi <58384462+siddharthkumarmamidi@users.noreply.github.com>
2020-06-07 14:51:59 +0530

    portfolio project has been done

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,13 @@ class Blog(models.Model):
     pub_date=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pretty_pub_date(self):
+        return self.pub_date.strftime('%b %e %Y')
 # Create your models here.
